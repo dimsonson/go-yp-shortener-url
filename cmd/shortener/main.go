f983f735e7203c1e7978219a4c15aaeeea782dae
dima_bo <dim.bod@gmail.com>
2022-08-29 20:51:48 +0300

    hame_290822

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+    "net/http"
+)
+
+// HelloWorld — обработчик запроса.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(`<!DOCTYPE html>
+	<html lang="ru">
+	  <head>
+		<meta charset="utf-8">
+		<title>Привет, мир!</title>
+	  </head>
+	  <body>
+		<h1>Привет, мир!</h1>
+		<p>Это веб-страница.</p>
+	 </body>
+	</html>`))
+}
+
+func main() {
+    // 
+	//маршрутизация запросов обработчику
+    http.HandleFunc("/", HelloWorld)
+    // запуск сервера с адресом localhost, порт 8080
+    http.ListenAndServe(":8080", nil)
+}
