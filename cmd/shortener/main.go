21d763a9069ab60ad7738167fa26bb9ab063ea68
dima_bo <dim.bod@gmail.com>
2022-08-30 15:15:55 +0300

    1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,20 +3,38 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
+
+var tpl = template.Must(template.ParseFiles("index.html"))
 
 // ShUrl — обработчик запроса.
 func ShUrl(w http.ResponseWriter, r *http.Request) {
+	
 	switch r.Method {
 	// если методом POST
 	case "GET":
+		tpl.Execute(w, r)
 		// выдаем строку
 		if err := r.ParseForm(); err != nil {
 			// если не заполнена, возвращаем код ошибки
 			http.Error(w, "Bad auth", 401)
 			return
 		}
+		w.Write(WebP1)
+		/* []byte(`<!DOCTYPE html>
+		<html lang="ru">
+		  <head>
+			<meta charset="utf-8">
+			<title>Привет, мир!</title>
+		  </head>
+		  <body>
+			<h1>Привет, мир!</h1>
+			<p>Это веб-страница.</p>
+		 </body>
+		</html>`)) */
 	case "POST":
+		tpl.Execute(w, r)
 		// проверяем форму
 		if err := r.ParseForm(); err != nil {
 			// если не заполнена, возвращаем код ошибки
@@ -65,3 +83,62 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	// продолжаем обработку запроса
 	// ...
 } */
+
+var WebP1 = []byte(`<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>undefined</title>
+  <meta name="generator" content="Google Web Designer 14.0.4.1108">
+  <style type="text/css" id="gwd-text-style">
+    p {
+      margin: 0px;
+    }
+    h1 {
+      margin: 0px;
+    }
+    h2 {
+      margin: 0px;
+    }
+    h3 {
+      margin: 0px;
+    }
+  </style>
+  <style type="text/css">
+    html, body {
+      width: 100%;
+      height: 100%;
+      margin: 0px;
+    }
+    body {
+      background-color: transparent;
+      transform: perspective(1400px) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+      transform-style: preserve-3d;
+    }
+    .gwd-input-1hwh {
+      position: absolute;
+      height: 1.79%;
+      width: 30%;
+      left: 34.53%;
+      top: 16.09%;
+    }
+    .gwd-p-1izk {
+      position: absolute;
+      width: 384.375px;
+      height: 37px;
+      font-family: Roboto;
+      font-weight: 500;
+      top: 122px;
+      left: 373px;
+    }
+  </style>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic|Roboto+Mono:100,200,300,regular,500,600,700,100italic,200italic,300italic,italic,500italic,600italic,700italic|Source+Sans+Pro:200,200italic,300,300italic,regular,italic,600,600italic,700,700italic,900,900italic|Raleway:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic|Noto+Sans:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic|Mukta:200,300,regular,500,600,700,800|Inter:100,200,300,regular,500,600,700,800,900" rel="stylesheet" type="text/css">
+</head>
+
+<body class="htmlNoPages">
+  <input type="text" id="text_1" class="gwd-input-1hwh">
+  <p class="gwd-p-1izk">Вставьте ссылку для создания короткой версии:</p>
+</body>
+
+</html>`)
