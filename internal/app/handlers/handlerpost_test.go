926ae3d189d76c050ddf5a77c39eaee2df9cb615
dima_bo <dim.bod@gmail.com>
2022-09-03 22:08:14 +0300

    1

--- internal/app/handlers/handlerpost_test.go
+++ internal/app/handlers/handlerpost_test.go
@@ -1,11 +1,42 @@
-package handlers
+package handlers_test
 
 import (
 	"net/http"
 	"testing"
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/handlers"
 )
 
 func TestPostHandler(t *testing.T) {
+	// определяем структуру теста
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+
+	type req struct {
+		metod string
+		endpoint string
+		body string
+	}
+	
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		req string
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "positive test #1",
+			want: want{
+				code:        200,
+				response:    `{"status":"ok"}`,
+				contentType: "application/json",
+			},
+		},
+	}
+	
 	type args struct {
 		w http.ResponseWriter
 		r *http.Request
