794b9e57a3d3f13df6696c475c010b542857ef96
dima_bo <dim.bod@gmail.com>
2022-09-05 19:20:03 +0300

    123

--- internal/app/httprouters/httprouter.go
+++ internal/app/httprouters/httprouter.go
@@ -1,14 +1,12 @@
 package httprouters
 
 import (
-	"net/http"
-
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewRouter() chi.Router { //chi.Router {
+func NewRouter() chi.Router {
 	rout := chi.NewRouter()
 
 	// зададим встроенные middleware, чтобы улучшить стабильность приложения
@@ -22,19 +20,4 @@ func NewRouter() chi.Router { //chi.Router {
 	rout.Post("/*", handlers.PostHandler)
 	//log.Fatal(http.ListenAndServe(":8080", rout))
 	return rout
-}
-
-func HTTPRouter(w http.ResponseWriter, r *http.Request) {
-	// определяем роутер chi
-	rout := chi.NewRouter()
-
-	// зададим встроенные middleware, чтобы улучшить стабильность приложения
-	//rout.Use(middleware.RequestID)
-	//rout.Use(middleware.RealIP)
-	rout.Use(middleware.Logger)
-	rout.Use(middleware.Recoverer)
-
-	rout.Get("/*", handlers.GetHandler)
-	rout.Post("/*", handlers.PostHandler)
-	rout.HandleFunc("/*", handlers.DefHandler)
 }
