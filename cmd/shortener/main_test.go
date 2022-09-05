68e9c181bd5c8c1814cc84579e522da41f547d76
dima_bo <dim.bod@gmail.com>
2022-09-05 18:27:06 +0300

    1

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,16 +1,46 @@
-package main
+package main_test
 
-import "testing"
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
+	r := chi.NewRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	resp, body := testRequest(t, ts, "GET", "/")
+	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
+	assert.Contains(t, "https://", body)
+
+	resp, _ = testRequest(t, ts, "POST", "/")
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	assert.Contains(t, "https://", body)
+
+	resp, _ = testRequest(t, ts, "PATCH", "/")
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	
+
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
 }
