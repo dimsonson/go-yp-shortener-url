97c9c459a6dd260f0a02522548808e9f69b9a6df
dima_bo <dim.bod@gmail.com>
2022-09-05 19:18:34 +0300

    12

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,16 +4,12 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dimsonson/go-yp-shortener-url/internal/app/handlers"
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/httprouters"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
 	httprouters.HTTPRouter()
 	log.Fatal(http.ListenAndServe(":8080", httprouters.NewRouter()))
-	
-}
 
+}
