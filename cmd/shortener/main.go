b983d4f1c47e389b791eda038d1490ec56a6cc7c
dima_bo <dim.bod@gmail.com>
2022-09-11 14:44:56 +0300

    2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,9 +9,8 @@ import (
 )
 
 func main() {
-
+	addr := ":8080"
 	httprouters.NewRouter()
-	fmt.Println()
-	log.Fatal(http.ListenAndServe(":8080", httprouters.NewRouter()))
-	
+	fmt.Printf("Started server on port %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, httprouters.NewRouter()))
 }
