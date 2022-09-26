959b9afa56480c693f405e32fe57f441ba54d87a
dima_bo <dim.bod@gmail.com>
2022-09-26 18:28:05 +0300

    chi compress

--- internal/app/httprouters/httprouter.go
+++ internal/app/httprouters/httprouter.go
@@ -26,13 +26,19 @@ func NewRouter(hn *handlers.Handler) chi.Router { // http.Handler {
 	// зададим встроенные middleware, чтобы улучшить стабильность приложения
 	rout.Use(middleware.Logger)
 	rout.Use(middleware.Recoverer)
-	//rout.Use(middleware.Compress(5))
+	rout.Use(middleware.Compress(1))
 	//rout.Use(middleware.Def)
 	//compressor := middleware.NewCompressor(5, "application/x-gzip")
 	//rout.Handle(CompressHandle())
 	//CompressHandle(rout)
 	//rout.Use(middleware.AllowContentEncoding())
 	//rout.Use(middleware.Gzip())
+	/* 	compressor := middleware.NewCompressor(5, "/*")
+	   	compressor.SetEncoder("gzip", func(w http.ResponseWriter, level int) io.Writer {
+
+
+	   		return w
+	   	})  */
 
 	// маршрут GET "/{id}" id в URL
 	rout.Get("/{id}", hn.HandlerGetShortURL)
