6e4666f5de54415873ce271f3ad9bb06c5381dc0
dima_bo <dim.bod@gmail.com>
2022-09-26 15:27:43 +0300

    application/x-gzip

--- internal/app/httprouters/httprouter.go
+++ internal/app/httprouters/httprouter.go
@@ -6,19 +6,19 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-/* var defaultCompressibleContentTypes = []string{
-    "text/html",
-    "text/css",
-    "text/plain",
-    "text/javascript",
-    "application/javascript",
-    "application/x-javascript",
+var defaultCompressibleContentTypes = []string{
+	"text/html",
+	"text/css",
+	"text/plain",
+	"text/javascript",
+	"application/javascript",
+	"application/x-javascript",
 	"application/x-gzip",
-    "application/json",
-    "application/atom+xml",
-    "application/rss+xml",
-    "image/svg+xml",
-} */
+	"application/json",
+	"application/atom+xml",
+	"application/rss+xml",
+	"image/svg+xml",
+}
 
 func NewRouter(hn *handlers.Handler) chi.Router {
 	// chi роутер
@@ -26,7 +26,11 @@ func NewRouter(hn *handlers.Handler) chi.Router {
 	// зададим встроенные middleware, чтобы улучшить стабильность приложения
 	rout.Use(middleware.Logger)
 	rout.Use(middleware.Recoverer)
-	rout.Use(middleware.Compress(5))
+	//rout.Use(middleware.Compress(5))
+	//rout.Use(middleware.Def)
+	compressor := middleware.NewCompressor(5, "application/x-gzip")
+
+	rout.Use(compressor.Handler)
 
 	// маршрут GET "/{id}" id в URL
 	rout.Get("/{id}", hn.HandlerGetShortURL)
