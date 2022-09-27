1d290a1418fda63a9b9c2040c3305d986bfdefb3
dimsonson <104103457+dimsonson@users.noreply.github.com>
2022-09-27 10:19:49 +0300

    Revert "Revert "Merge pull request #5 from dimsonson/incriment3_1_1""
    This reverts commit 98de75447d20de617828ae09b939c211e6c22fdb.

---
+++ internal/app/handlers/incorrectreq_test.go
@@ -0,0 +1,67 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/handlers"
+)
+
+func TestIncorrectRequests(t *testing.T) {
+	// определяем структуру теста
+	type want struct {
+		code int
+	}
+
+	type req struct {
+		metod    string
+		endpoint string
+		body     string
+	}
+
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		req  req
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "GET #1",
+			req: req{
+				metod:    "GET",
+				endpoint: "/xyz",
+				body:     "",
+			},
+			want: want{
+				code: 400,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+
+			//создаем тестирующий запрос
+			request := httptest.NewRequest(http.MethodGet, tt.req.endpoint, nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+
+			// определяем хендлер
+			h := http.HandlerFunc(handlers.IncorrectRequests)
+
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			resp := w.Result()
+			defer resp.Body.Close()
+			// проверяем код ответа
+			if resp.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+		})
+	}
+}
