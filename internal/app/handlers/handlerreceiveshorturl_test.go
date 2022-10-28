a5f7c2c5fe62095f73833b7b95c261f7948cc4c0
dimsonson <104103457+dimsonson@users.noreply.github.com>
2022-10-28 22:04:29 +0300

    tests fix3

--- internal/app/handlers/handlerreceiveshorturl_test.go
+++ internal/app/handlers/handlerreceiveshorturl_test.go
@@ -83,7 +83,7 @@ func TestHandlerGetShortURL(t *testing.T) {
 			h := handlers.NewHandler(srvs, "")
 			r := httprouters.NewRouter(h)
 			//	h := http.HandlerFunc(handlers.NewHandler())
-			s.PutToStorage(ctx, 1, "xyz", "https://pkg.go.dev/github.com/stretchr/testify@v1.8.0/assert#Containsf")
+			s.PutToStorage(ctx, "1", "xyz", "https://pkg.go.dev/github.com/stretchr/testify@v1.8.0/assert#Containsf")
 
 			rctx := chi.NewRouteContext()
 			rctx.URLParams.Add("id", strings.TrimPrefix(tt.req.endpoint, "/"))
