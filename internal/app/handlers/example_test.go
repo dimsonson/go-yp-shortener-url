65b0549de111e314eba0f6d4d2aaddcfc01a0f9b
dimsonson <104103457+dimsonson@users.noreply.github.com>
2022-12-31 14:06:20 +0300

    example

--- internal/app/handlers/example_test.go
+++ internal/app/handlers/example_test.go
@@ -1,4 +1,4 @@
-package handlers
+package handlers_test
 
 import (
 	"context"
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"strings"
 
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/handlers"
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/handlers/servicemock"
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/settings"
 	"github.com/go-chi/chi/v5"
@@ -14,9 +15,8 @@ import (
 
 var s = &servicemock.ServiceMock{}
 
-func Example() {
-
-	hPut := NewPutHandler(s, "http://localhost:8080")
+func ExamplePutHandler_Put() {
+	hPut := handlers.NewPutHandler(s, "http://localhost:8080")
 
 	wPut := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("http://localhost:8080/0xyz"))
@@ -39,74 +39,18 @@ func Example() {
 	hPut.PutBatch(wPutBatch, req)
 	fmt.Println(wPutBatch.Code)
 
-	hGet := NewGetHandler(s, "http://localhost:8080")
-
-	wGet := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodGet, "/{id}", nil)
-	// Создание контекста id пользователя для передачи хендлером в сервис.
-	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("id", strings.TrimPrefix("/xyz", "/"))
-	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
-	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
-	// запускаем хендлер
-	hGet.Get(wGet, req)
-	fmt.Println(wGet.Code)
-
-	wGetBatch := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodGet, "/api/user/urls", nil)
-	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
-	// запускаем хендлер
-	hGet.GetBatch(wGetBatch, req)
-	fmt.Println(wGetBatch.Code)
-
-	hDelete := NewDeleteHandler(s, "http://localhost:8080")
-
-	wDelete := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodGet, "/api/user/urls", nil)
-	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
-	// запускаем хендлер
-	hDelete.Delete(wDelete, req)
-	fmt.Print(wDelete.Code)
-
 	// Output:
 	// 201
 	// 201
 	// 201
-	// 307
-	// 200
-	// 202
+
 }
 
-func ExamplePut() {
-	s := &servicemock.ServiceMock{}
-
-	hPut := NewPutHandler(s, "http://localhost:8080")
-
-	wPut := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("http://localhost:8080/0xyz"))
-	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
-	// запускаем хендлер
-	hPut.Put(wPut, req)
-	fmt.Println(wPut.Code)
-
-	wPutJSON := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodPost, "/api/shorten", strings.NewReader(`{"url":"https://pkg.go.dev/io#Reader"}`))
-	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
-	// запускаем хендлер
-	hPut.PutJSON(wPutJSON, req)
-	fmt.Println(wPutJSON.Code)
-
-	wPutBatch := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodPost, "/api/shorten/batch", strings.NewReader(`[{"correlation_id": "05id", "original_url": "https://ya.ru/"}]`))
-	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
-	// запускаем хендлер
-	hPut.PutBatch(wPutBatch, req)
-	fmt.Println(wPutBatch.Code)
-
-	hGet := NewGetHandler(s, "http://localhost:8080")
+func ExampleGetHandler_Get() {
+	hGet := handlers.NewGetHandler(s, "http://localhost:8080")
 
 	wGet := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodGet, "/{id}", nil)
+	req := httptest.NewRequest(http.MethodGet, "/{id}", nil)
 	// Создание контекста id пользователя для передачи хендлером в сервис.
 	rctx := chi.NewRouteContext()
 	rctx.URLParams.Add("id", strings.TrimPrefix("/xyz", "/"))
@@ -123,20 +67,21 @@ func ExamplePut() {
 	hGet.GetBatch(wGetBatch, req)
 	fmt.Println(wGetBatch.Code)
 
-	hDelete := NewDeleteHandler(s, "http://localhost:8080")
+	// Output:
+	// 307
+	// 200
+}
+
+func ExampleDeleteHandler_Delete() {
+	hDelete := handlers.NewDeleteHandler(s, "http://localhost:8080")
 
 	wDelete := httptest.NewRecorder()
-	req = httptest.NewRequest(http.MethodGet, "/api/user/urls", nil)
+	req := httptest.NewRequest(http.MethodGet, "/api/user/urls", nil)
 	req = req.WithContext(context.WithValue(req.Context(), settings.CtxKeyUserID, "ok"))
 	// запускаем хендлер
 	hDelete.Delete(wDelete, req)
 	fmt.Print(wDelete.Code)
 
 	// Output:
-	// 201
-	// 201
-	// 201
-	// 307
-	// 200
 	// 202
 }
