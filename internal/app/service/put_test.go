d3554214ece7c4d95530e14247387017fcfa5a09
dimsonson <104103457+dimsonson@users.noreply.github.com>
2023-01-11 12:57:33 +0300

    test svc prepare

---
+++ internal/app/service/put_test.go
@@ -0,0 +1,70 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/service"
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/service/storagemock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPut(t *testing.T) {
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name          string
+		inputURL      string
+		InputUserid   string
+		expectedKey   string
+		expectedError error
+	}{
+		{
+			name:          "Positive test Put service level - OK",
+			inputURL:      "https://pkg.go.dev/io#Reader",
+			InputUserid:   "123",
+			expectedKey:   "8xyz9k",
+			expectedError: nil,
+		},
+		/* 		{
+		   			name:                  "Negativae test POST - Put long URL for short - server error",
+		   			inputMetod:            http.MethodPost,
+		   			inputEndpoint:         "/",
+		   			inputBody:             "https://pkg.go.dev/io#Reader",
+		   			inputUserID:           "srv",
+		   			expectedStatusCode:    http.StatusInternalServerError,
+		   			expectedResponseBody:  "http://localhost:8080/",
+		   			expectedHeader:        "Content-Type",
+		   			expectedHeaderContent: "text/plain; charset=utf-8",
+		   		},
+		   		{
+		   			name:                  "Negativae test POST - Put long URL for short - notUniq",
+		   			inputMetod:            http.MethodPost,
+		   			inputEndpoint:         "/",
+		   			inputBody:             "https://pkg.go.dev/io#Reader",
+		   			inputUserID:           "notUniq",
+		   			expectedStatusCode:    http.StatusConflict,
+		   			expectedResponseBody:  "http://localhost:8080/",
+		   			expectedHeader:        "Content-Type",
+		   			expectedHeaderContent: "text/plain; charset=utf-8",
+		   		},*/
+	}
+	s := &storagemock.StorageMock{}
+	svc := service.NewPutService(s, "")
+	ctx := context.Background()
+
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			key, err := svc.Put(ctx, "", "")
+
+			// проверяем код ответа
+			assert.Equal(t, tt.expectedKey, key)
+			// получаем и проверяем тело ответа
+			assert.Equal(t, tt.expectedError, err)
+			// проверка содержания строки в теле ответа
+/* 			assert.Containsf(t, string(key), tt.expectedResponseBody, "error message %s", "formatted")
+			// заголовок ответа
+			assert.Contains(t, (tt.expectedHeader), tt.expectedHeaderContent) */
+		})
+	}
+}
---
+++ internal/app/service/storagemock/mock.go
@@ -0,0 +1,98 @@
+// Package servicemock пакет заглушек обращения хендлеров к сервисам.
+package storagemock
+
+import (
+	"context"
+	"errors"
+
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/models"
+	"github.com/dimsonson/go-yp-shortener-url/internal/app/settings"
+)
+
+// ServiceMock структура заглушки обращения хендлеров к сервисам.
+type StorageMock struct {
+}
+
+// Put метод реализованный для запросов через заглушку.
+func (s *StorageMock) Put(ctx context.Context, key string, value string, userid string) (existKey string, err error) {
+	/* 	switch userid {
+	   	case "ok":
+	   		return "0xyz", nil
+	   	case "srv":
+	   		return "", errors.New("server error")
+	   	case "notUniq":
+	   		return "", errors.New(pgerrcode.UniqueViolation)
+	   	}
+	*/
+	return "", nil
+}
+
+// PutBatch метод реализованный для запросов через заглушку.
+func (s *StorageMock) PutBatch(ctx context.Context, dc models.BatchRequest, userid string) (dcCorr models.BatchRequest, err error) {
+	/* switch userid {
+	case "ok":
+		ec = []models.BatchResponse{{CorrelationID: "05id", ShortURL: "http://localhost:8080/0xyz"}}
+		return ec, nil
+	case "srv":
+		return nil, errors.New("server error")
+	case "notUniq":
+		return ec, errors.New(pgerrcode.UniqueViolation)
+	} */
+
+	return nil, nil
+}
+
+// Get метод реализованный для запросов через заглушку.
+func (s *StorageMock) Get(ctx context.Context, key string) (value string, del bool, err error) {
+	/* 	switch id {
+	   	case "xyz":
+	   		return "https://pkg.go.dev/io#Reader", false, nil
+	   	case "bad":
+	   		return "", false, errors.New("bad")
+	   	case "del":
+	   		return "", true, nil
+	   	} */
+	return "", true, err
+}
+
+// GetBatch метод реализованный для запросов через заглушку.
+func (s *StorageMock) GetBatch(ctx context.Context, userid string) (userURLs map[string]string, err error) {
+	/* 	kv := map[string]string{"xyz": "https://pkg.go.dev/io#Reader"}
+	   	switch userid {
+	   	case "ok":
+	   		return kv, nil
+	   	case "bad":
+	   		return nil, errors.New("noContent")
+	   	} */
+	return nil, err
+}
+
+// Ping метод реализованный для запросов через заглушку.
+func (s *StorageMock) Ping(ctx context.Context) (ok bool, err error) {
+	userid := ctx.Value(settings.CtxKeyUserID).(string)
+	switch userid {
+	case "ok":
+		return true, nil
+	case "bad":
+		return false, errors.New("DB error")
+	}
+	return true, nil
+}
+
+// Delete метод реализованный для запросов через заглушку.
+func (s *StorageMock) Delete(key string, userid string) (err error) {
+	return err
+}
+
+func (ms *StorageMock) Len(ctx context.Context) (lenn int) {
+
+	return lenn
+
+}
+
+func (ms *StorageMock) Load() {
+}
+
+func (ms *StorageMock) Close() {
+
+}
