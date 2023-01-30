b99a27bae97a2d9584b5d52410b436e0b0623179
dimsonson <104103457+dimsonson@users.noreply.github.com>
2023-01-30 10:07:21 +0300

    zerolog

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,11 +2,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	_ "net/http/pprof"
 
@@ -17,8 +20,15 @@ import (
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/service"
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/settings"
 	"github.com/dimsonson/go-yp-shortener-url/internal/app/storage"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/acme/autocert"
 )
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006/01/02 15:04:05"})
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+}
 
 // Константы по умолчанию.
 const (
@@ -38,7 +48,7 @@ var (
 
 func main() {
 	// Вывод данных о версии, дате, коммите сборки.
-	log.Printf("version=%s, date=%s, commit=%s\n", buildVersion, buildDate, buildCommit)
+	log.Printf("version=%s, date=%s, commit=%s", buildVersion, buildDate, buildCommit)
 
 	// Получаем переменные из флагов или переменных оркужения в структуру models.Config.
 	cfg := flagsVars()
@@ -62,15 +72,33 @@ func main() {
 	// Инциализация хендлеров.
 	r := httprouters.NewRouter(hPut, hGet, hDel, hPing)
 
+	// конфигурирование http сервера
+	httpsrv := &http.Server{Addr: cfg.ServerAddress, Handler: r}
+	// опередяляем контекст уведомления о сигнале прерывания
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// инциализация WaitGroup
+	var wg sync.WaitGroup
+	// добавляем счетчик горутины
+	wg.Add(1)
+	// запуск горутины shutdown http сервера
+	go httpServerShutdown(ctx, &wg, httpsrv)
+
 	// Запуск сервера.
-	log.Println("base URL:", settings.ColorGreen, cfg.BaseURL, settings.ColorReset)
+	log.Print("base URL:", settings.ColorGreen, cfg.BaseURL, settings.ColorReset)
+	// Выбор варианта запуска сервера http или https.
 	if cfg.EnableHTTPS {
-		log.Println("starting", settings.ColorBlue, "https", settings.ColorReset, "server on:", settings.ColorBlue, cfg.ServerAddress, settings.ColorReset)
-		log.Println(http.Serve(autocert.NewListener(cfg.ServerAddress), r))
-		return
+		log.Print("starting", settings.ColorBlue, "https", settings.ColorReset, "server on:", settings.ColorBlue, cfg.ServerAddress, settings.ColorReset)
+		log.Print(httpsrv.Serve(autocert.NewListener()))
+	} else {
+		log.Print("starting server on:", settings.ColorBlue, cfg.ServerAddress, settings.ColorReset)
+		log.Print(httpsrv.ListenAndServe())
 	}
-	log.Println("starting server on:", settings.ColorBlue, cfg.ServerAddress, settings.ColorReset)
-	log.Println(http.ListenAndServe(cfg.ServerAddress, r))
+	// остановка всех сущностей, куда передан контекст по прерыванию
+	stop()
+	// ожидаем выполнение горутин
+	wg.Wait()
+	// логирование закрытия сервера без ошибок
+	log.Print("http server gracefully shutdown")
 }
 
 // flagsVars парсинг флагов и валидация переменных окружения.
@@ -89,14 +117,14 @@ func flagsVars() (cfg models.Config) {
 	// проверяем наличие переменной окружения, если ее нет или она не валидна, то используем значение из флага
 	cfg.ConfigJSON, ok = os.LookupEnv("CONFIG")
 	if !ok && *cfgFlag != "" {
-		log.Println("eviroment variable CONFIG is empty or has wrong value ", cfg.ConfigJSON)
+		log.Print("eviroment variable CONFIG is empty or has wrong value ", cfg.ConfigJSON)
 		cfg.ConfigJSON = *cfgFlag
 	}
 	//
 	if cfg.ConfigJSON != "" {
 		configFile, err := os.ReadFile(*cfgFlag)
 		if err != nil {
-			log.Println("reading config file error:", err)
+			log.Print("reading config file error:", err)
 		}
 		if err == nil {
 			err = json.Unmarshal(configFile, &cfg)
@@ -111,7 +139,7 @@ func flagsVars() (cfg models.Config) {
 		cfg.ServerAddress = ServerAddress
 	}
 	if (!ok || !govalidator.IsURL(cfg.ServerAddress) || cfg.ServerAddress == "") && *addrFlag != "" {
-		log.Println("eviroment variable SERVER_ADDRESS is empty or has wrong value ")
+		log.Print("eviroment variable SERVER_ADDRESS is empty or has wrong value ")
 		cfg.ServerAddress = *addrFlag
 	}
 	// если нет флага или переменной окружения используем переменную по умолчанию
@@ -124,7 +152,7 @@ func flagsVars() (cfg models.Config) {
 		cfg.BaseURL = BaseURL
 	}
 	if (!ok || !govalidator.IsURL(cfg.BaseURL) || cfg.BaseURL == "") && *baseFlag != "" {
-		log.Println("eviroment variable BASE_URL is empty or has wrong value ")
+		log.Print("eviroment variable BASE_URL is empty or has wrong value ")
 		cfg.BaseURL = *baseFlag
 	}
 	// если нет флага или переменной окружения используем переменную по умолчанию
@@ -137,7 +165,7 @@ func flagsVars() (cfg models.Config) {
 		cfg.DatabaseDsn = DatabaseDsn
 	}
 	if !ok && *dlinkFlag != "" {
-		log.Println("eviroment variable DATABASE_DSN is not exist")
+		log.Print("eviroment variable DATABASE_DSN is not exist")
 		cfg.DatabaseDsn = *dlinkFlag
 	}
 	// проверяем наличие переменной окружения, если ее нет или она не валидна, то используем значение из флага
@@ -146,7 +174,7 @@ func flagsVars() (cfg models.Config) {
 		cfg.FileStoragePath = FileStoragePath
 	}
 	if !ok || (cfg.FileStoragePath == "" || !govalidator.IsUnixFilePath(cfg.FileStoragePath) || govalidator.IsWinFilePath(cfg.FileStoragePath)) {
-		log.Println("eviroment variable FILE_STORAGE_PATH is empty or has wrong value ")
+		log.Print("eviroment variable FILE_STORAGE_PATH is empty or has wrong value ")
 		cfg.FileStoragePath = *pathFlag
 	}
 	// проверяем наличие флага или пременной окружения для старта в https (tls)
@@ -157,7 +185,7 @@ func flagsVars() (cfg models.Config) {
 	}
 	// если нет флага или переменной окружения используем переменную по умолчанию
 	cfg.EnableHTTPS = defHTTPS
-	log.Println("eviroment variable ENABLE_HTTPS is empty or has wrong value ")
+	log.Print("eviroment variable ENABLE_HTTPS is empty or has wrong value ")
 	return cfg
 }
 
@@ -166,12 +194,12 @@ func newStrorageProvider(dlink, path string) (s service.StorageProvider) {
 	// если переменная SQL url не пустая, то используем SQL хранилище
 	if dlink != "" {
 		s = storage.NewSQLStorage(dlink)
-		log.Println("server will start with data storage "+settings.ColorYellow+"in PostgreSQL:", dlink, settings.ColorReset)
+		log.Print("server will start with data storage "+settings.ColorYellow+"in PostgreSQL:", dlink, settings.ColorReset)
 		return s
 	}
 	// иначе если есть path используем для хранения файл
 	if path != "" && (govalidator.IsUnixFilePath(path) || govalidator.IsWinFilePath(path)) {
-		log.Println("server will start with data storage " + settings.ColorYellow + "in file and memory cash" + settings.ColorReset)
+		log.Print("server will start with data storage " + settings.ColorYellow + "in file and memory cash" + settings.ColorReset)
 		log.Printf("file storage path: %s\n", path)
 		s = storage.NewFileStorage(make(map[string]string), make(map[string]string), make(map[string]bool), path)
 		s.Load()
@@ -179,6 +207,19 @@ func newStrorageProvider(dlink, path string) (s service.StorageProvider) {
 	}
 	// если переменная path не валидна, то используем память для хранения id:url
 	s = storage.NewMapStorage(make(map[string]string), make(map[string]string), make(map[string]bool))
-	log.Println("server will start with data storage " + settings.ColorYellow + "in memory" + settings.ColorReset)
+	log.Print("server will start with data storage " + settings.ColorYellow + "in memory" + settings.ColorReset)
 	return s
+}
+
+// httpServerShutdown реализует gracefull shutdown для ListenAndServe
+func httpServerShutdown(ctx context.Context, wg *sync.WaitGroup, srv *http.Server) {
+	// получаем сигнал о завершении приложения
+	<-ctx.Done()
+	// завершаем открытые соединения и закрываем http server
+	if err := srv.Shutdown(ctx); err != nil {
+		// логирование ошибки остановки сервера
+		log.Printf("HTTP server Shutdown error: %v", err)
+	}
+	// уменьшаем счетчик запущенных горутин
+	wg.Done()
 }
--- go.mod
+++ go.mod
@@ -14,7 +14,12 @@ require (
 	honnef.co/go/tools v0.3.3
 )
 
-require golang.org/x/net v0.5.0 // indirect
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+)
 
 require (
 	github.com/tommy-muehle/go-mnd/v2 v2.5.1
